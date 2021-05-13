4dd1095b77a787c7c9d95472bcda122face2cb95
Pinal-11 <dadhania.pinal@gmail.com>
2021-05-13 09:50:15 +0530

    initial commit

---
+++ mainapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Todo
+
+
+class Todo_list(admin.ModelAdmin):
+    list_display = ['added_date', 'text']
+
+
+admin.site.register(Todo, Todo_list)
